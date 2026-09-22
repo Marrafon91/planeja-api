ba44651cf71f774c6ef398b1d0f3ea057f3990f8
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-22 08:57:54 -0300

    Implementando os Mappers de DTO's com MapStruct

--- planeja-api/src/main/java/io/github/marrafon91/planeja_api/dominio/cartao/mapper/CartaoMapper.java
+++ planeja-api/src/main/java/io/github/marrafon91/planeja_api/dominio/cartao/mapper/CartaoMapper.java
@@ -3,8 +3,11 @@ package io.github.marrafon91.planeja_api.dominio.cartao.mapper;
 import io.github.marrafon91.planeja_api.dominio.cartao.dto.CartaoDetalhes;
 import io.github.marrafon91.planeja_api.dominio.cartao.dto.CartaoForm;
 import io.github.marrafon91.planeja_api.dominio.cartao.model.CartaoEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface CartaoMapper {
+
     CartaoEntity toEntity(CartaoForm form);
 
     CartaoDetalhes toDetalhes(CartaoEntity entity);
