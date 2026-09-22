6d053208d876cb292eb457534bbe1882b46b6517
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-22 07:55:43 -0300

    Mapeando a entidade de Cartões

---
+++ planeja-api/src/main/java/io/github/marrafon91/planeja_api/dominio/cartao/model/BandeiraCartao.java
@@ -0,0 +1,9 @@
+package io.github.marrafon91.planeja_api.dominio.cartao.model;
+
+public enum BandeiraCartao {
+    VISA,
+    MASTERCARD,
+    AMERICAN_EXPRESS,
+    DINNERS,
+    ELO
+}
---
+++ planeja-api/src/main/java/io/github/marrafon91/planeja_api/dominio/cartao/model/CartaoEntity.java
@@ -0,0 +1,38 @@
+package io.github.marrafon91.planeja_api.dominio.cartao.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_cartao")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CartaoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(nullable = false, name = "nome", length = 30)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "bandeira")
+    private BandeiraCartao bandeira;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDateTime.now());
+    }
+}
