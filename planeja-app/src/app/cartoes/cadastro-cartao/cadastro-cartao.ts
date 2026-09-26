363652f194fb9ac0ba47a79f7bf74842c0c7d289
Guilherme <guilherme.marrafon1@gmail.com>
2026-09-26 15:47:20 -0300

    Utilizando o modulo ReactiveForms do Angular para capturar valores digitados

--- planeja-app/src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
+++ planeja-app/src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+interface CadastroCartaoForm {
+  nome: FormControl<string>;
+  bandeira: FormControl<string>;
+}
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-cadastro-cartao',
   styleUrl: './cadastro-cartao.css',
   templateUrl: './cadastro-cartao.html',
 })
-export class CadastroCartao {}
+export class CadastroCartao implements OnInit {
+  form!: FormGroup<CadastroCartaoForm>;
+
+  ngOnInit(): void {
+    this.form = new FormGroup<CadastroCartaoForm>({
+      nome: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      bandeira: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.form.value);
+  }
+}
